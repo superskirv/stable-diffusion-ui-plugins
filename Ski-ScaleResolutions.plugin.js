0349e80e8f1e67e7801bdf8ebeee40d70bed9e37
superskirv <45948181+superskirv@users.noreply.github.com>
2022-12-14 18:39:16 -0800

    Update Ski-ScaleResolutions.plugin.js
    Cleaned up some code, made it easier to use settings.

--- Ski-ScaleResolutions.plugin.js
+++ Ski-ScaleResolutions.plugin.js
@@ -8,10 +8,10 @@
 (function () {
   //Comment out which ones you dont want to run.
 
-  // all_Resolutions( Mode, Max Resolution );
-  all_Resolutions('override',2048); //Adds all combinations of resolutions in increments of 64.
+  // all_Resolutions( Mode, Max Resolution, Min );
+  all_Resolutions('override',2048,256); //Adds all combinations of resolutions in increments of 64.
   // custom_scaled_resolutions( Mode, Max Size to match, Ratio(side divided by side), closeness percent, Min Size );
-  custom_scaled_resolutions('append',2048,768/512,0.03,14);
+  custom_scaled_resolutions('append',2048,768/512,0.03,384);
   // All options are optional. You can run as many times as you want with different setting.
 
   //---------------------------DO NOT EDIT BELOW------------------------------
@@ -32,31 +32,40 @@
   //    EX 1: .03 is within 3 percent, so for the ratio 1.5 it must be above 1.455 and below 1.545 (More Results)
   //    EX 2: 0 is ZERO. This will only match ratio's that are EXACTLY what you want. (Less results), None if you set min and max to restrictive.
   //  MIN SIZE:
-  //    The Smallest number of pixels to output a pair, reduces smaller resolution that match the ratio.
-  //    Formula is SIDE PLUS SIDE EQUALS (MIN SIZE), 8+8=16, or (8*64) + (8*64) = (16*64), or 512 + 512 = 1024
-  //    Ex 1: 16, will allow 448x576(7+9), or 384x640(6+10), or 704x320(11+5), if the pair matches the ratio.
-  //    Ex 2: 8, will allow 256x256(4+4), or 128x384(2+6), or 192x320(3+5), if the pair matches the ratio.
-  //    Generally its best to leave this above 12.
+  //    The Smallest number number for any side in pixels.
   //    If set correctly you can limit the results to just a few Pairs, which will make the drop down smaller.
 
   //---------------------------DO NOT EDIT BELOW------------------------------
-  function all_Resolutions(mode = 'append', size = 2048) {
+  function all_Resolutions(mode = 'append', max = 2048, min = 128) {
     //This will 'override' or 'append' the Resolutions for the the drop down box.
-    if (size % 64 != 0) {
+
+    if(max < 512){ max = 512; } //force minimum size.
+    if(min < 128){ min = 128; } //force minimum size.
+    //Making sure size is a multiple of 64
+    if (max % 64 != 0) {
       //If remaining numbers are greater than zero, fix it.
-      if(size % 64 >= 32){
-        //if More than 32 over a multiple of 64, find and add the remaining
-        size += 64-(size%64);
+      if(max % 64 >= 32) {
+        //if 32 or over a multiple of 64, find and add the remaining
+        max += 64-(max%64);
       } else {
         //if Less than 32 over a multiple of 64, subtract the remaining
-        size -= size%64;
+        max -= max%64;
       }
     }
-	  //These numbers are multiplied by 64
-    //Starts at 2 * 64 = 128
-	  var start = 2
-    //Ends at 32 * 64 = 2048 //36 * 64 = 2304
-	  var end = size/64;
+	  var end = max/64;
+
+    //Making sure min is a multiple of 64
+    if (min % 64 != 0) {
+      //If remaining numbers are greater than zero, fix it.
+      if(min % 64 >= 32) {
+        //if 32 or over a multiple of 64, find and add the remaining
+        min += 64-(min%64);
+      } else {
+        //if Less than 32 over a multiple of 64, subtract the remaining
+        min -= min%64;
+      }
+    }
+    start = min/64
 
 	  //Edit below at your own risk.
 	  var options="";
@@ -75,26 +84,41 @@
       document.getElementById('height').innerHTML = options;
     }
   }
-  function custom_scaled_resolutions( mode = 'append', size = 2048, size_ratio = 1.25, closeness = 0.03, min_size = 15) {
+  function custom_scaled_resolutions( mode = 'append', max_size = 2048, size_ratio = 1.25, closeness = 0.03, min_size = 128) {
 	  //This will Append Resolutions for the given side to the drop down box.
 
     //At some point I need to figure out how to add a button to the GUI and have these option pop up in a special menu.
 
+    if(max_size < 512){ max_size = 512; } //force minimum size.
+    if(min_size < 128){ min_size = 128; } //force minimum size.
     //This should force incorrect numbers to be a multiple of the closest 64 set.
-    if (size % 64 != 0) {
+    if (max_size % 64 != 0) {
       //If remaining numbers are greater than zero, fix it.
-      if(size % 64 >= 32){
-        //if More than 32 over a multiple of 64, find and add the remaining
-        size += 64-(size % 64);
+      if(max_size % 64 >= 32) {
+        //if 32 or over a multiple of 64, find and add the remaining
+        max_size += 64-(max_size % 64);
       } else {
         //if Less than 32 over a multiple of 64, subtract the remaining
-        size -= size % 64;
+        max_size -= max_size % 64;
       }
     }
+
+    //Making sure min is a multiple of 64
+    if (min_size % 64 != 0) {
+      //If remaining numbers are greater than zero, fix it.
+      if(min_size % 64 >= 32) {
+        //if 32 or over a multiple of 64, find and add the remaining
+        min_size += 64-(min_size%64);
+      } else {
+        //if Less than 32 over a multiple of 64, subtract the remaining
+        min_size -= min_size%64;
+      }
+    }
+
     var options_w=""; //The new width resolutions to add
     var options_h=""; //The new height resolutions to add
     var start = 2 //Used to start the loop
-    var end = size/64 //Used to end the loop
+    var end = max_size/64 //Used to end the loop
     var max = end*end //Used to Break the loops if it goes on to long.
 
     var pair = 1; //Used to help user pair up compatible resolutions
@@ -108,8 +132,8 @@
         if(num_loops > max){ break; } //prevents infinity loops
         //Speeding up search, reducing math calcs
         now = (((i*64) / (j*64)) / size_ratio);
-        //Reduces the number of smaller resolution pairs. (i + J, or 8+8= 512x512 image size, 7+9 = 448x576, etc...)
-        if( i + j >= min_size) {
+        //Must be higher than the minimum for any side.
+        if( i >= min_size || j  >= min_size) {
           if ( now >= (1-closeness) ) {
             if( now <= (1+closeness) ) {
               if(now == 1) { //Exact Match
@@ -126,7 +150,7 @@
       }
     }
     //if(pair == 1) {
-      //Informs the Users that the plugin failed to find compatible settings with the current settings.
+      //Informs the Users that the plugin failed to find compatible Pairs with the current settings.
       //options_w += '<option value="128">-Plugin Failed w-</option>';
       //options_h += '<option value="128">-Plugin Failed h-</option>';
     //}
